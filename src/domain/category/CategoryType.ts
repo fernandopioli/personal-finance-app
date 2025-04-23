5ee1d6b7308d949e3c76515d287752b8783646c9
Fernando Pioli <fernandohrp@gmail.com>
2025-04-23 12:56:08 -0300

    feat: create CategoryType class

---
+++ src/domain/category/CategoryType.ts
@@ -0,0 +1,26 @@
+import { ValueObject, Result } from '@domain/core'
+import { InvalidCategoryTypeError } from '@domain/category/errors'
+
+export class CategoryType extends ValueObject<string> {
+  private static readonly EXPENSE = 'expense'
+  private static readonly INCOME = 'income'
+
+  private constructor(value: string) {
+    super(value)
+  }
+
+  public static create(value: string): Result<CategoryType> {
+    const validationResult = this.validate(value)
+    if (validationResult.isFailure) {
+      return Result.fail(validationResult.errors)
+    }
+    return Result.ok(new CategoryType(value))
+  }
+
+  public static validate(value: string): Result<void> {
+    if (value === CategoryType.EXPENSE || value === CategoryType.INCOME) {
+      return Result.ok(undefined)
+    }
+    return Result.fail([new InvalidCategoryTypeError('type', value)])
+  }
+}
